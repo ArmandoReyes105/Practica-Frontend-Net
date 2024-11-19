f4eda7e100d9a1d3f0c397cb07eb49c092bd8500
ArmandoReyes105 <arrey105@outlook.com>
2024-11-19 14:27:55 -0600

    First commit

---
+++ Controllers/CarritoController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace frontendnet
+{
+    [Authorize(Roles = "Usuario")]
+    public class CarritoController() : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(); 
+        }
+    }
+}
